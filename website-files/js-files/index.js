b438d3bcc95d154c20102207f5ac1c60eeaa29c6
KeeganGrajek <Keegangrajek@gmail.com>
2022-09-12 15:51:40 -0700

    Added a few things (Mobilemenu)
    Added animation to button.
    Menu actually works, and javascript backend is a lot more simple so it handles the opening and closing of it properly and efficiently (swapping of the display style from "none" to "flex" and vis versa)
    
    Current bug, I need to fix the menu to not scroll but to size down when the screen size is smaller.
    
    What I want to add is a slide down of the dropdown menu to notify to people that the menu is open. More intuitive clues is key to making that work properly.

--- website-files/js-files/index.js
+++ website-files/js-files/index.js
@@ -5,73 +5,59 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function toggleMobileNavbar() {
-    var elm = document.getElementsByClassName("header-link-mobilemenu-dropdown");
-    var box = document.getElementsByClassName('body-box-5');
-    var box2 = document.getElementsByClassName('body-box-1');
-    var box3 = document.getElementsByClassName('body-about-box-1');
-    var box4 = document.getElementsByClassName('body-about-box-10');
-    var notifyBar = document.getElementsByClassName('notify-bar');
+    var elm1 = document.getElementsByClassName("mobilemenu-dropdown-body");
+    var elm2 = document.getElementsByClassName("body-div-1");
     try {
         if (!mobileNavbarState) {
-            console.log("uh-oh2");
-            console.log(mobileNavbarState);
-            elm[0].classList.toggle("header-link-mobilemenu-dropdown-display-not");
-            elm[0].classList.toggle("header-link-mobilemen-dropdown-display-yes");
-            document.getElementById('span-1').className = 'mobile-menu-bars-span-animation-1';
-            document.getElementById('span-2').className = 'mobile-menu-bars-span-animation-2';
-            document.getElementById('span-3').className = 'mobile-menu-bars-span-animation-3';
-            mobileNavbarState = true;
+            // console.log("uh-oh2");
+            // console.log(mobileNavbarState);
             try {
-                box[0].style.setProperty('display', 'none');
-            } catch { console.log("box")}
+                elm1[0].style.setProperty('display', 'flex');
+            } catch { console.log("elm1 error MNS false");}
             try {
-                box2[0].style.setProperty('display', 'none');
-            } catch { console.log("box2")}
+                elm2[0].style.setProperty('display', 'none');
+            } catch { console.log("elm1 error MNS false");}
             try {
-                box3[0].style.setProperty('display', 'none');
-            } catch { console.log("box3")}
-            try {
-                box4[0].style.setProperty('display', 'none');
-            } catch { console.log("box4")}
-            try {
-                box4[1].style.setProperty('display', 'none');
-            } catch { console.log("box4sub")}
-            try {
-                notifyBar[0].style.setProperty('display', 'none');
+            document.getElementById('span-1').classList.add("mobile-menu-bars-span-animation-1");
+            document.getElementById('span-2').classList.add("mobile-menu-bars-span-animation-2");
+            document.getElementById('span-3').classList.add("mobile-menu-bars-span-animation-3");
             } catch {
-                console.log("notify-bar");
+                console.log('error1');
             }
+            try {
+                document.getElementById('span-1').classList.remove("mobile-menu-bars-span-animation-1-not");
+                document.getElementById('span-2').classList.remove("mobile-menu-bars-span-animation-2-not");
+                document.getElementById('span-3').classList.remove("mobile-menu-bars-span-animation-3-not");
+            } catch {
+                console.log("error3");
+            }
+            mobileNavbarState = true;
             return;
         } 
         if (mobileNavbarState) {
-            console.log("uh-oh1");
-            console.log(mobileNavbarState);
-            elm[0].classList.toggle("header-link-mobilemenu-dropdown-display-not");
-            elm[0].classList.toggle("header-link-mobilemenu-dropdown-display-yes");
-            document.getElementById('span-1').className = 'mobile-menu-bars-span-animation-1-not';
-            document.getElementById('span-2').className = 'mobile-menu-bars-span-animation-2-not';
-            document.getElementById('span-3').className = 'mobile-menu-bars-span-animation-3-not';
-            mobileNavbarState = false;
+            // console.log("uh-oh1");
+            // console.log(mobileNavbarState);
             try {
-                box[0].style.setProperty('display', 'flex');
-            } catch { console.log("box")}
-            try { 
-                box2[0].style.setProperty('display', 'flex');
-            } catch { console.log("box2")}
+                elm1[0].style.setProperty('display', 'none');
+            } catch { console.log("elm1 error MNS true");}
             try {
-                box3[0].style.setProperty('display', 'flex');
-            } catch { console.log("box3")}
+                elm2[0].style.setProperty('display', 'flex');
+            } catch { console.log("elm1 error MNS true");}
             try {
-                box4[0].style.setProperty('display', 'flex');
-            } catch { console.log("box4")}
-            try {
-                box4[1].style.setProperty('display', 'flex');
-            } catch { console.log("box4sub")}
-            try {
-                notifyBar[0].style.setProperty('display', 'flex');
+                document.getElementById('span-1').classList.add("mobile-menu-bars-span-animation-1-not");
+                document.getElementById('span-2').classList.add("mobile-menu-bars-span-animation-2-not");
+                document.getElementById('span-3').classList.add("mobile-menu-bars-span-animation-3-not");
             } catch {
-                console.log("notify-bar");
+                console.log("error3");
             }
+            try {
+                document.getElementById('span-1').classList.remove("mobile-menu-bars-span-animation-1");
+                document.getElementById('span-2').classList.remove("mobile-menu-bars-span-animation-2");
+                document.getElementById('span-3').classList.remove("mobile-menu-bars-span-animation-3");
+            } catch {
+                console.log("error3");
+            }
+            mobileNavbarState = false;
             return;
         }
     } catch {
