4c07e11cf1d9bb8b7d9bfd4d73a0c5b274e9dc39
KeeganGrajek <Keegangrajek@gmail.com>
2022-11-10 22:13:17 -0800

    updated statistics even more

--- website-files/js-files/index.js
+++ website-files/js-files/index.js
@@ -33,6 +33,8 @@ document.addEventListener('DOMContentLoaded', function(){
     // setTimeout(function(){
     //     p[0].style.setProperty('display', 'none');
     // }, 2000+initial);
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (("0"+dt.getDate()).slice(-2)) +"/"+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()+1).slice(-2)) +":"+ (("0"+dt.getMinutes()+1).slice(-2));
 });
 
 function toggleMobileNavbar() {
